5e603c0d4e990f6dd832eb36a24a5d71e4e19ab4
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2018-05-13 00:31:53 +0700

    add UtilityTrait nb. move urlTitle to him, flashMessage and uniqueCode

--- FileTrait.php
+++ FileTrait.php
@@ -8,11 +8,7 @@
  * @created date 17 April 2018, 08:36 WIB
  * @link https://github.com/ommu/yii2-traits
  *
- * FileTrait berisi kumpulan fungsi yang berhubungan sistem file, seperti buat folder, hapus folder,
- * normalisasi nama file sehingga url friendly dll.
- *
  * Contains many function that most used :
- *	urlTitle
  *	formatFileType
  *	createUploadDirectory
  *
@@ -20,47 +16,8 @@
 
 namespace ommu\traits;
 
-trait FileTrait {
-	
-	/**
-	 * Create URL Title
-	 *
-	 * Takes a "title" string as input and creates a
-	 * human-friendly URL string with a "separator" string
-	 * as the word separator.
-	 *
-	 * @todo Remove old 'dash' and 'underscore' usage in 3.1+.
-	 * @param string $str Input string
-	 * @param string $separator Word separator (usually '-' or '_')
-	 * @param bool $lowercase  Wether to transform the output string to lowercase
-	 * @return string
-	 */
-	public function urlTitle($str, $separator = '-', $lowercase = true)
-	{
-		if($separator === 'dash')
-			$separator = '-';
-			
-		elseif($separator === 'underscore')
-			$separator = '_';
-
-		$qSeparator = preg_quote($separator, '#');
-		$trans = [
-			'&.+?:;' => '',
-			'[^a-z0-9 _-]' => '',
-			'\s+' => $separator,
-			'('.$qSeparator.')+' => $separator
-		];
-
-		$str = strip_tags($str);
-		foreach ($trans as $key => $val)
-			$str = preg_replace('#'.$key.'#i', $val, $str);
-
-		if ($lowercase === true)
-			$str = strtolower($str);
-
-		return trim(trim($str, $separator));
-	}
-
+trait FileTrait 
+{
 	/**
 	 * Explode or Implode Function
 	 *
