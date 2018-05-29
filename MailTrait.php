d7ec3bfe392333267d581369164dae464295aafa
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2018-05-30 03:11:31 +0700

    fixed mailTrait nb. add parse template, subject and body message

--- MailTrait.php
+++ MailTrait.php
@@ -9,7 +9,13 @@
  * @link https://github.com/ommu/yii2-traits
  *
  * Contains many function that most used :
- *	mailMessage
+ *	parseTemplate
+ *
+ *	getMailFrom
+ *	getMailTemplatePath
+ *	getMailMessage
+ *	parseMailSubject
+ *	parseMailBody
  *
  */
 
@@ -17,32 +23,114 @@ namespace ommu\traits;
 
 use Yii;
 use yii\helpers\Html;
+use app\models\CoreMailSetting;
 use app\models\CoreMailTemplate;
 
 trait MailTrait
 {
 	/**
-	 * mailMessage
+	 * getMailFrom
+	 *
+	 * @return array
 	 */
-	public function mailMessage($template, $attributes=null) 
+	public function getMailFrom()
+	{
+		$model = CoreMailSetting::find()
+			->select(['mail_name','mail_from'])
+			->where(['id' => 1])
+			->one();
+
+		if($model == null)
+			return false;
+		
+		$mail_name = $model->mail_name ? $model->mail_name : $model->mail_from;
+		return [$model->mail_from => $mail_name];
+	}
+
+	/**
+	 * getMailTemplatePath
+	 * 
+	 * @return string
+	 */
+	public function getMailTemplatePath()
 	{
 		$templatePath = join('/', [Yii::getAlias('@public'), 'email', 'template']);
-		
-		$templateCode = CoreMailTemplate::find()
-			->select(['template_code'])
+
+		return $templatePath;
+	}
+
+	/**
+	 * getMailMessage
+	 * 
+	 * @param string $template
+	 * @return string
+	 */
+	public function getMailMessage($template)
+	{
+		$template = $template.'.php';
+		$templateFile = join('/', [$this->getMailTemplatePath(), $template]);
+
+		$message = '';
+		if(file_exists($templateFile))
+			$message = file_get_contents($templateFile);
+
+		return $message;
+	}
+
+	/**
+	 * Method for parsing string
+	 * 
+	 * @param string $message Source string for parsing
+	 * @param array $attribute of example
+	 * [
+	 *		'{link}' => 'https://github.com/black-lamp/yii2-email-templates',
+	 *		'{author}' => Yii::$app->user->email
+	 *	]
+	 * 
+	 * @return string
+	 */
+	public function parseTemplate($message, $attribute)
+	{
+		foreach ($attribute as $key => $value) {
+			$message = strtr($message, [
+				'{'.$key.'}' => $value,
+			]);
+		}
+
+		return $message;
+	}
+
+	/**
+	 * Parsing of email subject
+	 * 
+	 * @return string
+	 */
+	public function parseMailSubject($template) 
+	{
+		$model = CoreMailTemplate::find()
+			->select(['subject'])
 			->where(['template' => $template])
 			->one();
 		
-		$messageBody = file_get_contents(join('/', [$templatePath, $templateCode->template_code.'.php']));
-		$messageSearch = $messageReplace = [];
-		if($attributes && is_array($attributes) && !empty($attributes)) {
-			foreach ($attributes as $key => $val) {
-				$messageSearch[] = '{'.$key.'}';
-				$messageReplace[] = $val;
-			}
-			return str_ireplace($messageSearch, $messageReplace, $messageBody);
+		$message = $this->parseTemplate($model->subject, ['sitename' => Yii::$app->name]);
+		
+		return $message;
+	}
 
-		} else 
-			return $messageBody;
+	/**
+	 * Parsing of email body
+	 * 
+	 * @return string
+	 */
+	public function parseMailBody($template, $attributes=null) 
+	{
+		$model = CoreMailTemplate::find()
+			->select(['template_file'])
+			->where(['template' => $template])
+			->one();
+		
+		$message = $this->parseTemplate($this->getMailMessage($model->template_file), $attributes);
+		
+		return $message;
 	}
 }
